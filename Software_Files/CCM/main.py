c74fcc717fe91159d065e99e51e582fe67b7d7af
Aswinpraabu Anbarasan <aswinaswinapp@gmail.com>
2025-06-24 20:09:03 -0400

    Added code to call the init and cyclic tasks from submodules

--- Software_Files/CCM/main.py
+++ Software_Files/CCM/main.py
@@ -8,10 +8,15 @@ from time import sleep_ms, ticks_ms, ticks_diff
 # this module is responsible for controlling the car's hardware components
 
 def initialize():
-	pass
+
+	hw_drivers.initialize()  # Initialize hardware drivers
+	rf_comms.initialize()  # Initialize radio communications
+	
 
 def task_005ms():
-	pass
+	#print("Running Main 5ms task...")
+	rf_comms.task_005ms()
+
 
 def task_010ms():
 	pass
@@ -25,9 +30,12 @@ if __name__ == "__main__":
 	timer_ref_005ms = timer_ref_010ms = timer_ref_100ms = ticks_ms()
 
 	while True:
+		#print("Main loop running... {0}ms".format(ticks_diff(ticks_ms(), timer_ref_005ms)))
 		if ticks_diff(ticks_ms(), timer_ref_005ms) >= 5:
+			#print("5ms task executed")
 			timer_ref_005ms = ticks_ms() # Reset the 5ms timer reference
 			task_005ms()
+			#print("5ms task time: {0}ms".format(ticks_diff(ticks_ms(), timer_ref_005ms)))
 		if ticks_diff(ticks_ms(), timer_ref_010ms) >= 10:
 			timer_ref_010ms = ticks_ms() # Reset the 10ms timer reference
 			task_010ms()
