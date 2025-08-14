f33a73382d1c28fbdaf34049dc62b63782a4a16f
Aswinpraabu Anbarasan <aswinaswinapp@gmail.com>
2025-08-14 00:14:10 -0400

    reorganized the way rx messages are decoded. And added a diagnostic to detect LOC with remote controller

--- Software_Files/CCM/CCM_Submodules/rf_comms.py
+++ Software_Files/CCM/CCM_Submodules/rf_comms.py
@@ -7,11 +7,65 @@ RCM_RF_ADDR = 0x22
 RADIO_FREQ_MHZ = 433
 rfm69:RFM69 = None # type: ignore
 
+msg_0x0A_last_rx_time = 0 # Time of last successful reception of Propulsion Control message
+
+class signal_class():
+    name = ""
+    factor = 1.0
+    offset = 0.0
+    min = 0.0
+    max = 0.0
+    default = 0.0
+    start_bit = 0
+    length = 0
+    cycle_time = 0  # in milli seconds, used for periodic signals. -1 means not periodic
+    def __init__(self,signal_name, signal_factor,signal_offset, signal_min, signal_max, signal_default, signal_start_bit, signal_length):
+        self.name = signal_name
+        self.factor = signal_factor
+        self.offset = signal_offset
+        self.min = signal_min
+        self.max = signal_max
+        self.default = signal_default
+        self.start_bit = signal_start_bit
+        self.length = signal_length
+
+class message_class():
+    msg_id = 0x00
+    msg_len = 0
+    signals = []
+    def __init__(self, message_id, message_length, message_signals:list[signal_class]):
+        self.msg_id = message_id
+        self.msg_len = message_length
+        self.signals = message_signals
+
+    def add_signal(self, new_signal:signal_class):
+        #self.signals.append(new_signal)
+        pass
+    def remove_signal(self, signal_name:str):
+        #self.signals = [signal for signal in self.signals if signal.name != signal_name]
+        pass
+    def validate(self):
+        return True
+        # Check that signals do not overlap and fit within message length
+        bit_map = [0] * (self.msg_len * 8)
+        for signal in self.signals:
+            for i in range(signal.start_bit, signal.start_bit + signal.length):
+                if i >= len(bit_map) or bit_map[i] == 1:
+                    return False
+                bit_map[i] = 1
+        return True
+
+
+
+# Set to store valid message IDs
+# format: (MSG_ID, MSG_LEN)
+valid_msg_set = set()
 
 def initialize():
     print("Initializing RFM69 radio communications...")
 
     global rfm69
+    global msg_0x0A_last_rx_time
 
     # Initialize RFM radio
     rfm69_SPI, rfm69_CS, rfm69_RESET = hw_drivers_data.get_rfm69_info()
@@ -19,6 +73,11 @@ def initialize():
     rfm69.high_power = True  # Only for RFM69HW!
     rfm69.node = CCM_RF_ADDR
     rfm69.destination = RCM_RF_ADDR
+
+    #TODO: initialize this set in another file. That file should also contain the message decoding functions
+    valid_msg_set.add((0x0A, 3))  # Propulsion Control message
+
+    msg_0x0A_last_rx_time = ticks_ms() 
     
 def rf_receive():
     t1 = ticks_ms()
@@ -28,15 +87,16 @@ def rf_receive():
     packet = rfm69.receive(timeout=5, keep_listening=True)
     
     if packet is not None:
-        turn_angle, throttle, decode_result = decode_packet(packet)
+        packet_arbitration(packet)
 
         # Update global data
-        rf_comms_data.rx_turn_angle = turn_angle
-        rf_comms_data.rx_throttle = throttle
-        rf_comms_data.rx_validity = decode_result
+        #rf_comms_data.rx_turn_angle = turn_angle
+        #rf_comms_data.rx_throttle = throttle
+        #rf_comms_data.rx_validity = decode_result
         
         #packet_text = str(packet, "ascii")
-        #print("Received rx_turn_angle: {0}, rx_throttle: {1}, rx_validity: {2}, diff: {3}".format(turn_angle, throttle, decode_result,ticks_diff(ticks_ms(), t1)))
+        #print("Received : {0} {1} {2}".format(packet[0], packet[1], packet[2]))
+        
     
     else:
         #print("Received nothing! Listening again...")
@@ -44,10 +104,29 @@ def rf_receive():
 
     return rf_comms_data.rx_validity
     
-def decode_packet(packet):
+def packet_arbitration(packet):
+    '''
+    Packet format: [MSG_ID,RAW_DATA]\n
+    '''
+    global msg_0x0A_last_rx_time
+    msg_id:int = packet[0]
+    #print("Packet ID: {0}".format(msg_id))
+    # Include checksum verification later
+    if((msg_id, len(packet)) in valid_msg_set):
+        raw_data = packet[1:]
+        if msg_id == 0x0A:
+            msg_0x0A_last_rx_time = ticks_ms()  # Update last received time for Propulsion Control message #DIAG
+            decode_msg_0x0A_PropulsionCtrl(raw_data)
+    else:
+        print("Invalid packet received: {0}".format(packet))
+        rf_comms_data.rx_validity = False
+    
+
+
+def decode_msg_0x0A_PropulsionCtrl(raw_data):
     '''
     Decodes a received packet into its components.
-    Packet format: [MSG_ID, TURN_ANGLE_RAW, THROTTLE_RAW]\n
+    Raw Data format: [TURN_ANGLE_RAW, THROTTLE_RAW]\n
     turn_angle = TURN_ANGLE_RAW - 90\n
     throttle = THROTTLE_RAW\n
     **Returns**:
@@ -55,20 +134,39 @@ def decode_packet(packet):
     -   throttle (int): percentage
     -   decode_result (bool): True if packet is valid, False otherwise
     '''
-    # Include checksum verification later
-    MSG_LEN = 3
-    
-    if len(packet) == MSG_LEN:
-        turn_angle = int(packet[1]) - 90
-        throttle = int(packet[2])
+    turn_angle = int(raw_data[0]) - 90
+    throttle = int(raw_data[1])
+    decode_result = True
+
+    print("Received rx_turn_angle: {0}, rx_throttle: {1}, rx_validity: {2}".format(turn_angle, throttle, decode_result))
+
+    #TODO: add range checks
+    if decode_result:
+        rf_comms_data.rx_turn_angle = turn_angle
+        rf_comms_data.rx_throttle = throttle
+        rf_comms_data.rx_validity = decode_result
+
         decode_result = True
     else:
         turn_angle = 0
         throttle = 0
         decode_result = False
-    
-    return turn_angle, throttle, decode_result
 
+def rf_comms_diagnostics():
+
+    time_diff = ticks_diff(ticks_ms(), msg_0x0A_last_rx_time)
+    #print("Diff: ", time_diff)
+    if time_diff > 100: # 100ms timeout
+        rf_comms_data.LOC_with_RCM = True
+        print("CCM has lost communication with RCM!")
+    else:
+        rf_comms_data.LOC_with_RCM = False
+        
+        
 
 def task_005ms():
     rf_receive()
+
+
+def task_010ms():
+    rf_comms_diagnostics()
