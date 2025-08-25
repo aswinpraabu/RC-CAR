c46963ded986fd1edfa4b12d083a2ef954d11c22
Aswinpraabu Anbarasan <aswinaswinapp@gmail.com>
2025-08-25 19:59:58 -0400

    declaring all the calibrations as constants

--- Software_Files/CCM/CCM_Submodules/calibrations.py
+++ Software_Files/CCM/CCM_Submodules/calibrations.py
@@ -13,16 +13,16 @@ from CCM_Libraries.ina260 import AveragingCount, ConversionTime
 
 class HW_DRIVERS_Class:
     # Servo calibration parameters
-    CAL_f_servo_freq = 50  # Frequency for servo PWM in Hz
-    CAL_a_servo_min_angle = -90  # Minimum angle for servo in degrees
-    CAL_t_servo_min_duty_offset_ms = -0.5344   # Offset for minimum duty cycle in milliseconds
-    CAL_t_servo_max_duty_offset_ms = 0.57  # Offset for maximum duty cycle in milliseconds
+    CAL_f_servo_freq = const(50)  # Frequency for servo PWM in Hz
+    CAL_a_servo_min_angle = const(-90)  # Minimum angle for servo in degrees
+    CAL_t_servo_min_duty_offset_ms = const(-0.5344)   # Offset for minimum duty cycle in milliseconds
+    CAL_t_servo_max_duty_offset_ms = const(0.57)  # Offset for maximum duty cycle in milliseconds
 
     # DC Motor calibration parameters
-    CAL_f_dc_motor_freq = 1000  # Frequency for DC motor PWM in Hz
-    CAL_P_dc_motor_min_power = -100  # Minimum power for DC motor in percentage
-    CAL_P_dc_motor_max_power = 100  # Maximum power for DC motor in percentage
-    CAL_p_dc_motor_reverse_factor = 50  # Minimum speed for DC motor in RPM
+    CAL_f_dc_motor_freq = const(1000)  # Frequency for DC motor PWM in Hz
+    CAL_P_dc_motor_min_power = const(-100)  # Minimum power for DC motor in percentage
+    CAL_P_dc_motor_max_power = const(100)  # Maximum power for DC motor in percentage
+    CAL_p_dc_motor_reverse_factor = const(50)  # Minimum speed for DC motor in RPM
 
     # INA260 calibration parameters
     CAL_t_ina260_voltage_conversion_time = ConversionTime.TIME_8_244_ms  # Voltage conversion time in ms
@@ -30,11 +30,11 @@ class HW_DRIVERS_Class:
     CAL_n_ina260_averaging_count = AveragingCount.COUNT_4  # Number of samples to average
 
 class CONTROLS_Class:
-    CAL_t_Prepare_Shutdown_duration_ms = 5000  # Duration to stay in Prepare_Shutdown mode
+    CAL_t_Prepare_Shutdown_duration_ms = const(5000)  # Duration to stay in Prepare_Shutdown mode
 
 class RF_COMMS_Class:
-    CAL_f_rf_comms_freq = 1000  # Frequency for RF communications in Hz
-    CAL_t_shutdown_timeout_ms = 100  # Timeout for reseting the shutdown request in milliseconds
+    CAL_f_rf_comms_freq = const(1000)  # Frequency for RF communications in Hz
+    CAL_t_shutdown_timeout_ms = const(100)  # Timeout for reseting the shutdown request in milliseconds
 
 HW_DRIVERS_Calibrations = HW_DRIVERS_Class()
 CONTROLS_Calibrations = CONTROLS_Class()
