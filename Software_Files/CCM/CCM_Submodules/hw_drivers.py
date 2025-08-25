ca21f4cf0ecc90605356c3d63b53adfbd900e58a
Aswinpraabu Anbarasan <aswinaswinapp@gmail.com>
2025-08-25 19:47:57 -0400

    RC-76 Initialize the INA260 sensor

--- Software_Files/CCM/CCM_Submodules/hw_drivers.py
+++ Software_Files/CCM/CCM_Submodules/hw_drivers.py
@@ -128,6 +128,13 @@ propulsion_motor = DC_Motor()
 
 def initialize():
     print("Initializing hardware drivers...")
+    global ina260_sensor
+    i2c_obj = I2C(0, scl=Pin(I2C_SCL_PIN), sda=Pin(I2C_SDA_PIN))
+    ina260_sensor = INA260(i2c_obj, INA260_ADDR)
+    ina260_sensor.voltage_conversion_time = CAL.CAL_t_ina260_voltage_conversion_time
+    ina260_sensor.current_conversion_time = CAL.CAL_t_ina260_current_conversion_time
+    ina260_sensor.averaging_count = CAL.CAL_n_ina260_averaging_count
+
     hw_drivers_data.rfm69_SPI = SPI(
                                     0,
                                     baudrate=500_000,
