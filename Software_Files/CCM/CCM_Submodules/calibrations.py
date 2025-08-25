ae3326c58e721141d505ec42cab3c753995436f3
Aswinpraabu Anbarasan <aswinaswinapp@gmail.com>
2025-08-25 19:49:05 -0400

    RC-76 adding calibrations for INA260

--- Software_Files/CCM/CCM_Submodules/calibrations.py
+++ Software_Files/CCM/CCM_Submodules/calibrations.py
@@ -6,6 +6,7 @@
 # t = time
 
 from micropython import const
+from CCM_Libraries.ina260 import AveragingCount, ConversionTime
 
 # This unnecessarily creates classes, but it helps with organization and clarity
 # If more efficiency is needed, these can be converted global constants instead
@@ -22,6 +23,11 @@ class HW_DRIVERS_Class:
     CAL_P_dc_motor_min_power = -100  # Minimum power for DC motor in percentage
     CAL_P_dc_motor_max_power = 100  # Maximum power for DC motor in percentage
     CAL_p_dc_motor_reverse_factor = 50  # Minimum speed for DC motor in RPM
+
+    # INA260 calibration parameters
+    CAL_t_ina260_voltage_conversion_time = ConversionTime.TIME_8_244_ms  # Voltage conversion time in ms
+    CAL_t_ina260_current_conversion_time = ConversionTime.TIME_1_1_ms # Current conversion time in ms
+    CAL_n_ina260_averaging_count = AveragingCount.COUNT_4  # Number of samples to average
 
 class CONTROLS_Class:
     CAL_t_Prepare_Shutdown_duration_ms = 5000  # Duration to stay in Prepare_Shutdown mode
