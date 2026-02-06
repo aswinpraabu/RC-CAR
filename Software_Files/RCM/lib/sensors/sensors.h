a2fab16896d1a25861ecec208f2c1ba96a197d9f
Aswinpraabu Anbarasan <aswinaswinapp@gmail.com>
2026-02-06 00:05:09 -0500

    Updated PINs for the pots

--- Software_Files/RCM/lib/sensors/sensors.h
+++ Software_Files/RCM/lib/sensors/sensors.h
@@ -4,8 +4,8 @@
 
 
 #define STATUS_LED PB10 // LED1
-#define TURN_POT_PIN PA0 // potentiometer pin for Turn Angle input
-#define THROTTLE_POT_PIN PA3 // potentiometer pin for Throttle input
+#define TURN_POT_PIN PA2 // potentiometer pin for Turn Angle input
+#define THROTTLE_POT_PIN PA1 // potentiometer pin for Throttle input
 
 
 #define RFM69_CS    PB8  // b8
