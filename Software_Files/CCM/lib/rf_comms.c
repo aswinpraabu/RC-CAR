5392259502fab9acf8ce842bdb1e97ef2cef7795
Aswinpraabu Anbarasan <aswinaswinapp@gmail.com>
2026-03-15 20:51:48 -0400

    RC-130 Implemented functions for enabling/disabling interupts on Core0

--- Software_Files/CCM/lib/rf_comms.c
+++ Software_Files/CCM/lib/rf_comms.c
@@ -3,14 +3,20 @@
 
 
 // module interface, platform specific
-void noInterrupts();                // function to disable interrupts
-void interrupts();                  // function to enable interrupts
+void noInterrupts()                // function to disable interrupts
+{
+    core0_interrupts = save_and_disable_interrupts();
+}
+void interrupts()                  // function to enable interrupts
+{
+    restore_interrupts(core0_interrupts);
+}
 // function to control the GPIO tied to RFM69 chip select (parameter HIGH or LOW)
 void RFM69_SetCSPin(bool value)
 {
     gpio_put(RFM69_CS_PIN, !value); // Active LOW
 }
-bool RFM69_ReadDIO0Pin(void);       // function to read GPIO connected to RFM69 DIO0 (RFM69 interrupt signalling)
+// function to read GPIO connected to RFM69 DIO0 (RFM69 interrupt signalling)
 bool RFM69_ReadDIO0Pin(void)
 {
     return gpio_get(RFM69_DIO0_PIN);
--- Software_Files/CCM/lib/rf_comms.h
+++ Software_Files/CCM/lib/rf_comms.h
@@ -1,4 +1,8 @@
 #include "RFM69.h"
 #include "pin_config.h"
 #include "pico/stdlib.h"
+#include "hardware/sync.h"
+
+
+volatile uint32_t core0_interrupts = 0; // variable to save and restore interrupts state RFM69 moduled
 
