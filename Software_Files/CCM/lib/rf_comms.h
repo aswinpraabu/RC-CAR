76cafcccd696e54a8fd249f4b5ff3292c47860a9
Aswinpraabu Anbarasan <aswinaswinapp@gmail.com>
2026-04-02 07:35:50 -0400

    Fix NETWORK_ID definition to match the default SYNC WORD of the RH_RFM69 library and correct typo in gpio0 interrupt callback function

--- Software_Files/CCM/lib/rf_comms.h
+++ Software_Files/CCM/lib/rf_comms.h
@@ -9,7 +9,7 @@
 
 #define CCM_NODE_ADDR 0x11
 #define RCM_NODE_ADDR 0x22
-#define NETWORK_ID 0xAA2D
+#define NETWORK_ID 0xD42D
 
 
 
@@ -18,11 +18,13 @@ int64_t _RFM69_timer_callback(alarm_id_t id, __unused void *user_data);
 
 void rfm69_SPI_init(void);
 void rfm69_device_init(void);
-void rfm69_gpio0_interupt_callback(uint gpio, uint32_t events);
+void rfm69_gpio0_interrupt_callback(uint gpio, uint32_t events);
+void rfm69_reset(void);
 
 void rf_comms_init(void);
 void rf_comms_task_005ms(void);
 void rf_comms_task_010ms(void);
 void rf_comms_task_100ms(void);
+void rf_comms_task_debug(void);
 
 #endif // RF_COMMS_H
