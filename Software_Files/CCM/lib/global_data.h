a6d337692a6522f62cf19c09bb0a4eb1067892c0
Aswinpraabu Anbarasan <aswinaswinapp@gmail.com>
2026-05-03 09:45:27 -0400

    adding global data store for main controls

--- Software_Files/CCM/lib/global_data.h
+++ Software_Files/CCM/lib/global_data.h
@@ -5,13 +5,32 @@
 #include "stdio.h"
 #include "stdint.h"
 
+enum ccm_power_state {
+    CCM_POWERSTATE_SHUTDOWN = 0,
+    CCM_POWERSTATE_RUNNING = 1,
+    CCM_POWERSTATE_PRE_SHUTDOWN = 2
+};
+
 struct rf_comms_data
 {
     int8_t rx_throttle;
     int8_t rx_turn_angle;
     uint8_t rx_shutdown_flag;
+    bool rx_validity_flag;
+};
+
+struct controls_data
+{
+    int8_t car_throttle;
+    int8_t car_turn_angle;
+    
+    bool led1_bat_low;
+    bool led2_loc_rcm;
+
+    enum ccm_power_state ccm_power_state; 
 };
 
 extern struct rf_comms_data global_rf_comms_data;
+extern struct controls_data global_controls_data;
 
 #endif //GLOBAL_DATA_H
