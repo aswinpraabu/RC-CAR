b16d2ea0f2ee7afbce3567f61c1771b00cec1f2d
Aswinpraabu Anbarasan <aswinaswinapp@gmail.com>
2026-05-03 09:55:06 -0400

    added propulsion control based on rf data

--- Software_Files/CCM/lib/controls.c
+++ Software_Files/CCM/lib/controls.c
@@ -13,12 +13,24 @@ void controls_init(void)
 
 void motor_control(void)
 {
-    // Placeholder for motor control logic
+    bool comms_data_check = global_rf_comms_data.rx_validity_flag;
+    bool power_mode_check = global_controls_data.ccm_power_state == CCM_POWERSTATE_RUNNING;
+    //bool safety_check = hw_drivers_data.battery_voltage_low_critical_diag.get_status() != DIAG_STATUS.FAIL;
+    bool safety_check = true; // Placeholder for safety check, replace with actual diagnostic status check
+    bool drive_enable = comms_data_check && power_mode_check && safety_check;
+
+    if(drive_enable) {
+        global_controls_data.car_throttle = global_rf_comms_data.rx_throttle;
+        global_controls_data.car_turn_angle = global_rf_comms_data.rx_turn_angle;
+    } else {
+        global_controls_data.car_throttle = 0;
+        global_controls_data.car_turn_angle = 0;
+    }
 }
 
 void power_control(void)
 {
-    // Placeholder for power control logic
+    
 }
 
 void warning_lights_control(void)
